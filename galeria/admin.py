094f8a3494b3749dcb5c656e7db6c560362696ef
Bruno Duran <bgdbruno@gmail.com>
2023-05-27 10:14:37 -0300

    Finalizando o módulo "Django: persistência de dados e Admin"

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda', 'descricao', 'foto', "publicado" )
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 10
+    list_editable = ('publicado',)
+    list_filter = ('categoria',)
+
+admin.site.register(Fotografia, ListandoFotografias)
